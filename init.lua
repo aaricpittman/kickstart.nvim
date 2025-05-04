d5b7bf7fccb19ca7e6e155725974d05433c338e5
Aaric Pittman <aaricpittman@gmail.com>
2025-05-04 18:05:42 -0500

    feat: add chatgpt.nvim

--- init.lua
+++ init.lua
@@ -228,6 +228,30 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   'github/copilot.vim',
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('chatgpt').setup {
+        api_key_cmd = 'op read op://private/OpenAI/NEOVIM_API_KEY --no-newline',
+        openai_params = {
+          model = 'gpt-4-1106-preview',
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 4095,
+          temperature = 0.2,
+          top_p = 0.1,
+          n = 1,
+        },
+      }
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim', -- optional
+      'nvim-telescope/telescope.nvim',
+    },
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
